8f23c08a3d4187b062a54a2ae3a476bc31539300
Michael Kielbasinski <85803354+MiniKielbyM@users.noreply.github.com>
2023-05-04 16:32:24 +0000

    visual stuff

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ io.on('connection', function (client) {
     console.log(data);
   });
   client.on('messages', function (data) {
-    var splitData = data.split('<br/>')
+    var splitData = data.split('<br')
     var x = splitData.length - 2;
     var y = splitData[x]
     if (y.indexOf(">") > ! -1 || y.indexOf("<") > ! -1 || splitData.length > 2) {
